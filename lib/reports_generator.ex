a214674ec4a546cbb443471a66fc841f04218623
Enzo Vieira <enzovieira16@hotmail.com>
2022-05-24 01:10:04 +0100

    Credo setup

---
+++ lib/reports_generator.ex
@@ -0,0 +1,18 @@
+defmodule ReportsGenerator do
+  @moduledoc """
+  Documentation for `ReportsGenerator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ReportsGenerator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
