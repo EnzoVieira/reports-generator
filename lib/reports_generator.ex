aa91cc78a42e16260b1bc3622b0c1ca0aa1a0e5d
Enzo Vieira <enzovieira16@hotmail.com>
2022-06-05 21:40:21 +0100

    Extraindo o parser para um novo módulo
    Novos conceitos: alias, Stream.map

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,24 +1,13 @@
 defmodule ReportsGenerator do
+  alias ReportsGenerator.Parser
+
   def build(filename) do
-    "reports/#{filename}"
-    |> File.stream!()
-    |> Enum.reduce(report_acc(), fn line, acc ->
-      [id, food_name, price] = parse_line(line)
-      Map.put(acc, id, acc[id] + price)
-    end)
-
-    # |> Enum.map(&parse_line/1) # Outra forma de escrever a linha anterior
-    # |> Enum.map(&parse_line(&1)) # Outra forma de escrever a linha anterior
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), &sum_values(&1, &2))
   end
 
-  defp parse_line(line) do
-    line
-    |> String.trim()
-    |> String.split(",")
-    |> List.update_at(2, &String.to_integer/1)
-
-    # |> List.update_at(2, fn elem -> String.to_integer(elem) end) # Outra forma de escrever a linha anterior
-  end
+  defp sum_values([id, _food_name, price], acc), do: Map.put(acc, id, acc[id] + price)
 
   defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
