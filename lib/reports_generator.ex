feb3876704457308fc2849079685d07fb8132786
Enzo Vieira <enzovieira16@hotmail.com>
2022-06-05 19:04:20 +0100

    Somando os valores dos pedidos
    Novos conceitos aprendidos: Enum.into, Enum.reduce, Map.put, 1..30, &{funcao(&chave), &valor}

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,10 +1,24 @@
 defmodule ReportsGenerator do
   def build(filename) do
     "reports/#{filename}"
-    |> File.read()
-    |> handle_file()
+    |> File.stream!()
+    |> Enum.reduce(report_acc(), fn line, acc ->
+      [id, food_name, price] = parse_line(line)
+      Map.put(acc, id, acc[id] + price)
+    end)
+
+    # |> Enum.map(&parse_line/1) # Outra forma de escrever a linha anterior
+    # |> Enum.map(&parse_line(&1)) # Outra forma de escrever a linha anterior
   end
 
-  defp handle_file({:ok, file_content}), do: file_content
-  defp handle_file({:error, _reason}), do: "Error while opening file"
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
+
+    # |> List.update_at(2, fn elem -> String.to_integer(elem) end) # Outra forma de escrever a linha anterior
+  end
+
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
