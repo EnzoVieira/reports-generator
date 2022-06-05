0c8e8837140b24dd01837565d82cccf1b25fda4a
Enzo Vieira <enzovieira16@hotmail.com>
2022-06-05 21:43:49 +0100

    Retornando o maior valor
    Novos conceitos: Enum.max_by

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -7,6 +7,8 @@ defmodule ReportsGenerator do
     |> Enum.reduce(report_acc(), &sum_values(&1, &2))
   end
 
+  def fetch_higher_cost(report), do: Enum.max_by(report, fn {_key, value} -> value end)
+
   defp sum_values([id, _food_name, price], acc), do: Map.put(acc, id, acc[id] + price)
 
   defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
