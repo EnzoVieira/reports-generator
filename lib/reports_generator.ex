f0a8df876b91dff0e9dd029fc8263f944520291f
Enzo Vieira <enzovieira16@hotmail.com>
2022-11-26 21:16:48 +0000

    Refatorando o retorno do maior preço

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -12,13 +12,19 @@ defmodule ReportsGenerator do
     "sushi"
   ]
 
+  @options ["foods", "users"]
+
   def build(filename) do
     filename
     |> Parser.parse_file()
     |> Enum.reduce(report_acc(), &sum_values(&1, &2))
   end
 
-  def fetch_higher_cost(report), do: Enum.max_by(report, fn {_key, value} -> value end)
+  def fetch_higher_cost(report, option) when option in @options do
+    {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
+  end
+
+  def fetch_higher_cost(_report, _option), do: {:error, "Invalid option"}
 
   defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
     users = Map.put(users, id, users[id] + price)
